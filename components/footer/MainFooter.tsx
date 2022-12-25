15e33bcaa53090d17dfaefc48ff3e327a7bb9656
bban2 <bandn119@gmail.com>
2022-12-25 21:39:58 +0700

    update footer

--- components/footer/MainFooter.tsx
+++ components/footer/MainFooter.tsx
@@ -1,8 +1,12 @@
 import styles from './footer.module.css'
+import {Typography} from "antd";
+
 export default function MainFooter() {
     return (
-        <div className={styles.wrapper}>
-            Created by banban @2023
+        <div>
+            <Typography.Text>
+                Created by banban | Jan, 2023
+            </Typography.Text>
         </div>
     )
 }
