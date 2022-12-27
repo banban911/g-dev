5273419778581f00c028e83479c5bbeada67cc6c
Nguyễn Duy Bân <80823953+banban911@users.noreply.github.com>
2022-12-27 11:04:03 +0700

    update

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,120 +1,132 @@
-import {useDispatch, useSelector} from "react-redux";
-import {Fragment, ReactElement, useEffect, useState} from 'react';
-import axios from 'axios';
-import {Button, Card, Checkbox, Form, Input, notification, Space, Typography} from 'antd';
-import {useRouter} from "next/router";
-import auth, {selectAuthState, setAuthState} from '../../src/store/auth';
+import { useDispatch, useSelector } from "react-redux";
+import { Fragment, ReactElement, useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Button,
+  Card,
+  Checkbox,
+  Form,
+  Input,
+  notification,
+  Space,
+  Typography,
+} from "antd";
+import { useRouter } from "next/router";
+import auth, { selectAuthState, setAuthState } from "../../src/store/auth";
 import SpinCircle from "../../components/spin/SpinCircle";
-import {LockOutlined, UserOutlined} from "@ant-design/icons";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import useTrans from "../hooks/useTrans";
 
 export default function Login() {
-    type NotificationType = 'success' | 'info' | 'warning' | 'error';
+  type NotificationType = "success" | "info" | "warning" | "error";
 
-    interface loginInfo {
-        username: string,
-        password: string
+  interface loginInfo {
+    username: string;
+    password: string;
+  }
+
+  const [api, contextHolder] = notification.useNotification();
+  const [loading, setLoading] = useState(false);
+  const trans = useTrans();
+
+  const loginErrorNotif = (type?: NotificationType) => {
+    api["error"]({
+      message: trans.login.fail,
+      description: trans.login.loginFailMsg,
+    });
+  };
+
+  const authState = useSelector(selectAuthState);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const login = async (values: loginInfo) => {
+    const { username, password } = values;
+    const res = await axios.post(`http://localhost:3000/api/auth/login`, {
+      username,
+      password,
+    });
+    return res;
+  };
+
+  const onFinish = (values: loginInfo) => {
+    setLoading(true);
+    setTimeout(async () => {
+      const res = await login(values);
+      if (res) {
+        localStorage.setItem("token", JSON.stringify(res.data.token));
+      }
+      dispatch(setAuthState(true));
+      setLoading(false);
+    }, 300);
+  };
+
+  useEffect(() => {
+    if (authState) {
+      router.push("/posts/dashboard");
     }
+  }, [authState]);
 
-    const [api, contextHolder] = notification.useNotification();
-    const [loading, setLoading] = useState(false)
-    const trans = useTrans()
+  const onFinishFailed = async (errorInfo: any) => {};
+  return loading ? (
+    <SpinCircle />
+  ) : (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: `calc(100vh - 64px)`,
+      }}
+    >
+      {contextHolder}
+      <Card
+        title={<div style={{ textAlign: "center" }}>Welcome back.</div>}
+        bordered={false}
+        loading={loading}
+        style={{ width: "33vw", minWidth: "400px", maxWidth: "500px" }}
+      >
+        <Form
+          name='basic'
+          initialValues={{ username: "", password: "" }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete='on'
+        >
+          <Form.Item
+            name='username'
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input placeholder='Username' prefix={<UserOutlined />} />
+          </Form.Item>
 
-    const loginErrorNotif = (type?: NotificationType) => {
-        api['error']({
-            message: trans.login.fail,
-            description: trans.login.loginFailMsg
-        });
-    };
+          <Form.Item
+            name='password'
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder='Password' prefix={<LockOutlined />} />
+          </Form.Item>
 
-    const authState = useSelector(selectAuthState)
-    const dispatch = useDispatch()
-    const router = useRouter()
+          <Form.Item>
+            <Space style={{ width: "100%", justifyContent: "space-between" }}>
+              <Checkbox>Remember me</Checkbox>
+              <Typography.Link className='login-form-forgot' href=''>
+                Forgot password
+              </Typography.Link>
+            </Space>
+          </Form.Item>
 
-    const login = async (values: loginInfo) => {
-        const {username, password} = values
-        const res = await axios.post(`http://localhost:3000/api/auth/login`, {
-            username,
-            password
-        })
-        return res
-    }
-
-    const onFinish = (values: loginInfo) => {
-        setLoading(true)
-        setTimeout(async () => {
-            const res = await login(values)
-            if (res) {
-                localStorage.setItem('token', JSON.stringify(res.data.token))
-            }
-            dispatch(setAuthState(true))
-            setLoading(false)
-        }, 300)
-
-    }
-
-    useEffect(() => {
-        if (authState) {
-            router.push('/posts/dashboard')
-        } else {
-            loginErrorNotif()
-        }
-
-    }, [authState])
-
-    const onFinishFailed = async (errorInfo: any) => {
-    }
-    return (
-        loading ? <SpinCircle/> :
-            <div
-                style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: `calc(100vh - 64px)`}}>
-                {contextHolder}
-                <Card title={<div style={{textAlign: 'center'}}>Welcome back.</div>} bordered={false} loading={loading}
-                      style={{width: '33vw', minWidth: '400px', maxWidth: '500px'}}>
-                    <Form
-                        name="basic"
-                        initialValues={{username: '', password: ''}}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="on"
-
-                    >
-                        <Form.Item
-                            name="username"
-                            rules={[{required: true, message: 'Please input your username!'}]}
-                        >
-                            <Input placeholder='Username' prefix={<UserOutlined/>}/>
-                        </Form.Item>
-
-                        <Form.Item
-                            name="password"
-                            rules={[{required: true, message: 'Please input your password!'}]}
-
-                        >
-                            <Input.Password placeholder='Password' prefix={<LockOutlined/>}/>
-                        </Form.Item>
-
-                        <Form.Item>
-                            <Space style={{width: '100%', justifyContent: 'space-between'}}>
-                                <Checkbox>Remember me</Checkbox>
-                                <Typography.Link className="login-form-forgot" href="">
-                                    Forgot password
-                                </Typography.Link>
-                            </Space>
-                        </Form.Item>
-
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" block>
-                                Log in
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </Card>
-            </div>)
+          <Form.Item>
+            <Button type='primary' htmlType='submit' block>
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
 
 Login.getLayout = function getLayout(page: ReactElement) {
-    return (
-        <Fragment>{page}</Fragment>
-    )
-}
+  return <Fragment>{page}</Fragment>;
+};
--- src/store/posts.ts
+++ src/store/posts.ts
@@ -1,89 +1,85 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {HYDRATE} from "next-redux-wrapper";
-import {PostType} from "../../pages/posts/dashboard";
-import {PostApi} from "../services/post.service";
-import {AppState} from "./index";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { HYDRATE } from "next-redux-wrapper";
+import { PostType } from "../../pages/posts/dashboard";
+import { PostApi } from "../services/post.service";
+import { AppState } from "./index";
 import axios from "axios";
 
 export interface PostsState {
-    post: PostType[],
-    loading: boolean,
-    error: string | undefined
+  post: PostType[];
+  loading: boolean;
+  error: string | undefined;
 }
 
 //api to fetch initial data
-export const getPosts = (params: any) => createAsyncThunk(
-    'post/getPosts',
-    async (thunkAPI) => {
-        const result = await axios.get(`https://dummyjson.com/posts?limit=${params.limit}`)
-        if (result) {
-            return result.data.posts
-        }
-    }
-)
+export const getPosts = createAsyncThunk("post/getPosts", async (thunkAPI) => {
+  const result = await axios.get(`https://dummyjson.com/posts?limit=12`);
+  if (result) {
+    return result.data.posts;
+  }
+});
 
 // Initial state
 
 const initialState: PostsState = {
-    post: [],
-    loading: false,
-    error: ''
-}
+  post: [],
+  loading: false,
+  error: "",
+};
 
 export const postSlice = createSlice({
-    name: 'post',
-    initialState,
-    reducers: {
-
-        // delete posts
-        deletePost(state, action) {
-            state.post = state.post.filter(p => p.id !== action.payload.id)
-        },
-
-        // edit post
-        editPost(state, action) {
-            const updatedPost = action.payload
-            state.post = state.post.map((item) => {
-                if (item.id === action.payload.id) {
-                    return updatedPost
-                }
-                return item
-            })
-        },
-
-        //create post
-        createPost(state, action) {
-            state.post.push(action.payload)
-        },
+  name: "post",
+  initialState,
+  reducers: {
+    // delete posts
+    deletePost(state, action) {
+      state.post = state.post.filter((p) => p.id !== action.payload.id);
     },
 
-    extraReducers: (builder) => {
-        builder
-            .addCase(getPosts.pending, (state, action) => {
-                state.loading = true
-            })
-            .addCase(getPosts.fulfilled, (state, action) => {
-                // Add posts to the state array
-                state.loading = false
-                state.post = action.payload
-                state.error = ''
-            })
-            .addCase(getPosts.rejected, (state, action) => {
-                state.loading = false
-                state.post = []
-                state.error = action.error.message
-            })
-            .addCase(HYDRATE, (state, action) => {
-                return {
-                    ...state,
-                }
-            })
-    }
-})
+    // edit post
+    editPost(state, action) {
+      const updatedPost = action.payload;
+      state.post = state.post.map((item) => {
+        if (item.id === action.payload.id) {
+          return updatedPost;
+        }
+        return item;
+      });
+    },
 
-export const createPost = postSlice.actions.createPost
-export const deletePost = postSlice.actions.deletePost
-export const editPost = postSlice.actions.editPost
+    //create post
+    createPost(state, action) {
+      state.post.push(action.payload);
+    },
+  },
 
-export const selectPostState = (state: AppState) => state.post
-export default postSlice.reducer
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPosts.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(getPosts.fulfilled, (state, action) => {
+        // Add posts to the state array
+        state.loading = false;
+        state.post = action.payload;
+        state.error = "";
+      })
+      .addCase(getPosts.rejected, (state, action) => {
+        state.loading = false;
+        state.post = [];
+        state.error = action.error.message;
+      })
+      .addCase(HYDRATE, (state, action) => {
+        return {
+          ...state,
+        };
+      });
+  },
+});
+
+export const createPost = postSlice.actions.createPost;
+export const deletePost = postSlice.actions.deletePost;
+export const editPost = postSlice.actions.editPost;
+
+export const selectPostState = (state: AppState) => state.post;
+export default postSlice.reducer;
